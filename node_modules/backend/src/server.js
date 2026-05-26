f45a1d0e7db893c6474a4ecd6daa587d16877f65
Sapper <saniamykhailenko@gmail.com>
2026-05-26 21:44:14 +0300

    init monorepo and init backend

---
+++ node_modules/backend/src/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import { errors } from "celebrate";
+import "dotenv/config";
+
+import { connectMongoDB } from "./db/connectMongoDB.js";
+import { logger } from "./middleware/logger.js";
+import { notFoundHandler } from "./middleware/notFoundHandler.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+
+// import authRoutes from "./routes/authRoutes.js";
+
+const app = express();
+const PORT = process.env.PORT ?? 3030;
+
+app.use(logger);
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+// app.use(authRoutes);
+
+app.use(notFoundHandler);
+
+app.use(errors());
+
+app.use(errorHandler);
+
+await connectMongoDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
