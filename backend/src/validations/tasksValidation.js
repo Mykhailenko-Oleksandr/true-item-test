dae8fd2c66731358cf1efeda0f0381b6bd6ad23a
Sapper <saniamykhailenko@gmail.com>
2026-05-27 22:38:05 +0300

    upd: backend - getAllTasksSchema

--- backend/src/validations/tasksValidation.js
+++ backend/src/validations/tasksValidation.js
@@ -16,7 +16,7 @@ export const getAllTasksSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string(),
+    tag: Joi.string().trim().allow(""),
     q: Joi.string().trim().allow(""),
   }),
 };
